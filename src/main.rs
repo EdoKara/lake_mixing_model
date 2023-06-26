6917333182a87522fa3b34af4320b80eda64d704
= <=>
2023-06-26 08:43:51 -0400

    added plotting; modified function for better opera tion.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,46 @@
 
-fn main() {
+use plotters::prelude::*;
+use iter_num_tools::lin_space;
 
-let t_initial:f64 = 150.700;
-let latitude:f64 = 45.0;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
 
-let angle = sun_angle(&t_initial, &latitude);
-let rad = solar_radiation(&t_initial, &latitude);
+    let t_initial = lin_space(0.0..=10.0, 1000);
+    let latitude = 39.2;
+    
+    let mut angle: Vec<f64> = Vec::new();
+    let mut rad: Vec<f64> = Vec::new();
+    let mut ar: Vec<(f64,f64)> = Vec::new();
 
-println!("Solar radiation is {rad} watts per meter squared. sun angle at this time is {angle} degrees.")    
+    for val in t_initial { 
+        angle.push(sun_angle(&val, &latitude));
+        rad.push(solar_radiation(&val, &latitude));
+        ar.push((val, solar_radiation(&val, &latitude)));
+    };
+
+    let root = BitMapBackend::new("./test.png", 
+    (1920, 1080)).into_drawing_area();
+    root.fill(&WHITE)?;
+    let mut chart = ChartBuilder::on(&root)
+        .build_cartesian_2d(0f64..10f64, 0f64..1400f64)?;
+
+    chart.configure_mesh().draw()?;
+
+    chart
+        .draw_series(LineSeries::new(ar.iter().cloned(), &BLACK))?;
+
+    chart
+        .configure_series_labels()
+        .background_style(WHITE.mix(0.8))
+        .border_style(BLACK)
+        .draw()?;
+
+    root.present()?;
+
+    Ok(())
 }
+
+    
+
 
 fn solar_radiation(t_initial: &f64, latitude:&f64) -> f64 {
 //calculate the incident solar radiation for a lake based upon the time of the year. 
@@ -17,17 +49,17 @@ fn solar_radiation(t_initial: &f64, latitude:&f64) -> f64 {
 
 //this boils down mostly to angle.
 
-const PI:f64 = 3.1415926;
+const PI:f64 = std::f64::consts::PI;
 const CF:f64 = 180.0/PI; //conversion factor between radians and degrees.
 
 
-let θ:f64 = sun_angle(&t_initial, &latitude);
+let θ:f64 = sun_angle(t_initial, latitude);
 
 let airmass:f64 = 1_f64/(θ.cos()*CF);
 
 let radiation:f64 = 1353.0 * (0.7_f64).powf((airmass).powf(0.678_f64));
 
-return radiation
+radiation
 }
 
 fn sun_angle(t_initial:&f64, latitude:&f64) -> f64{
@@ -35,16 +67,16 @@ fn sun_angle(t_initial:&f64, latitude:&f64) -> f64{
 //the definitions I've seen for elevation angle of the sun for time are based on time past the spring equinox. 
 //I'm going to define it in hours for some simplicity.
 
-const PI:f64 = 3.1415926;
+const PI:f64 = std::f64::consts::PI;
 const CF:f64 = 180.0/PI; //conversion factor between radians and degrees.
 
-let sun_angle:f64 = 360.0*((t_initial*24.0) % 24.0);
+let sun_angle:f64 = 360.0*(t_initial % 24.0);
 
 let horiz_angle:f64 = 23.5*((360_f64*(t_initial/365.25)).sin()*CF);
 
 let η:f64 = 90.0 - ((
-    ((&latitude.cos())*(horiz_angle.cos())*(sun_angle.cos())) + 
-    ((horiz_angle.sin())*(&latitude.sin()))
+    latitude.cos()*(horiz_angle.cos()*(sun_angle.cos())) + 
+    ((horiz_angle.sin())*(latitude.sin()))
 ).acos()
 )*CF;
 
