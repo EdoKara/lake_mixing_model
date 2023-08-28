2d99c9e963fb0bdd9e1340bd7d78776d6a06ea5f
Andrew Brown <brown.ajb@gmail.com>
2023-08-28 16:04:32 -0400

    Started working on select lake shape fn

--- src/box_pack/mod.rs
+++ src/box_pack/mod.rs
@@ -1,9 +1,10 @@
 pub mod box_pack{
     use shapefile::record::polygon::GenericPolygon;
     use shapefile::Error;
+    use std::io::ErrorKind;
 
     
-    pub fn input_lake_shapes(filepath:&str) -> Result<Vec<GenericPolygon<shapefile::Point>>,Error> {
+    pub fn input_lake_shapes(filepath:&str) -> Result<Vec<GenericPolygon<shapefile::Point>>, Error> {
 
         /* This function takes an inpute filepath (stringslice) and uses the shapefile package to read
         the assumed shapefile. It either returns a vector of shapes or an error, depending on whether it
@@ -13,8 +14,23 @@ pub mod box_pack{
 
         let reader = shapefile::ShapeReader::from_path(filepath)?;
         let polygons = reader.read_as::<shapefile::Polygon>()?;
-        return Ok(polygons)
+
+        return Ok(polygons) 
+
+        
+
     }
 
 
+    pub fn select_lake_shape(index:usize, inputresult: Result<Vec<GenericPolygon<shapefile::Point>>, Error>)  {
+
+        let test = match inputresult{
+            Ok(_) => inputresult,
+            Err(error) => Err(error)
+        };
+
+        
+    
+    }
 }
+
